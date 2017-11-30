fcc5236a1c410d8ede160e3e07d4ddbea7ff3141
Wassim Marrakchi <marrakchiwassim@yahoo.fr>
2017-11-30 01:57:10 -0500

    r

--- Google/popup.js
+++ Google/popup.js
@@ -6,20 +6,22 @@ $(function(){
     // Retrieves number of websites already blocked
     chrome.storage.sync.get('number', function(blocks){
       var number = 0;
-      if(blocks.total)
-      {
-        number += parseInt(budget.total);
+      if(blocks.total) {
+        number += parseInt(blocks.total);
       }
-    })
 
-    // Add the website to the list of blocked websites and increment by one the
-    // number of websites blocked
-    var url = $('#url').val();
-    if(url){
-      blocked.append(string(url));
-      number++;
-    }
-    $('#number').text(number);
-    $('#url').text('');
-  })
+      // Add the website to the list of blocked websites and increment by one the
+      // number of websites blocked
+      var url = $('#url').val();
+      if(url){
+        blocked.append(string(url));
+        number++;
+      }
+
+      chrome.storage.sync.set({'number': number});
+
+      $('#number').text(number);
+      $('#url').text('');
+    });
+  });
 });
