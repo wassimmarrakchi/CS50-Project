d580e8811c6e5849c020991f0e71bc473a4fb3da
Evan <evan1041@me.com>
2017-12-01 00:44:46 -0500

    Background js file

--- Google/popup.js
+++ Google/popup.js
@@ -58,18 +58,4 @@ $(function(){
       });
     });
   })
-
-  // Redirects blocked websites
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.storage.sync.get(['number', 'websites'], function(oldNumber){
-      let number = parseInt(oldNumber.number);
-      let websites = JSON.parse(oldNumber.websites);
-      for(let i = 0; i < number; i++) {
-        if (changeInfo.url == websites[i]) {
-          chrome.tabs.update(null, {url:"http://en.wikipedia.org"});
-          console.log("Url blocked: ", tab.url);
-        }
-      }
-    });
-  });
 });
