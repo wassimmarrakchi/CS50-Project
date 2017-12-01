7bf6328d42e881229e5f61c418ea7c3c052399c4
Wassim Marrakchi <marrakchiwassim@yahoo.fr>
2017-12-01 07:57:42 -0500

    e

--- Google/background.js
+++ Google/background.js
@@ -1,19 +1,25 @@
 // Redirects blocked websites
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)
+{
   console.log("ping at", tab.url)
-  chrome.storage.sync.get(toString(tab.url), function(block){
-  	if (block != true) {
-      chrome.storage.sync.get(['number', 'websites'], function(oldNumber){
+  chrome.storage.sync.get(toString(tab.url), function(block)
+  {
+  	if (block != true)
+    {
+      chrome.storage.sync.get(['number', 'websites'], function(oldNumber)
+      {
         let number = parseInt(oldNumber.number);
         let websites = JSON.parse(oldNumber.websites);
-        for(let i = 0; i < number; i++) {
-          if (tab.url == websites[i]) {
+        for(let i = 0; i < number; i++)
+        {
+          if (tab.url == websites[i])
+          {
             chrome.storage.sync.set({'visited' : tab.url});
             chrome.tabs.update(null, {url:chrome.extension.getURL('flashcard.html')});
             console.log("Url blocked: ", tab.url);
-          }
-        }
+          };
+        };
       });
-    }
+    };
   });
 });
