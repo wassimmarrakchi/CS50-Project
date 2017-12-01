2bd7207b4924e19eae8e5c9a91fa3a32f3bc8876
Evan <evan1041@me.com>
2017-12-01 06:55:41 -0500

    flashcard functionality

--- Google/flashcards.js
+++ Google/flashcards.js
@@ -1,50 +1,80 @@
 let pile = [];
 let correct_answer;
 let correct;
+let question;
+let unordered_cards = {};
+let total_correct = 0;
+let splice;
+let old_url;
+chrome.storage.sync.get('visited', function(block){
+	old_url = block.visited;
+});
+
+chrome.storage.sync.set({old_url : 'false'});
 
 // Generate and display new question
 function ask(pile) {
-		let question = pile[Math.floor(Math.random() * pile.length)];
-		pile.splice(question, 1);
+		let location = Math.floor(Math.random() * pile.length);
+		splice = location;
+		question = pile[location];
 		$("#question").text(question);
+		correct_answer = unordered_cards[question];
+		console.log("question: ", question);
+		console.log("correct answer: ", correct_answer);
 }
 
 // Flashcard functionality
 $(function(){
-	//Generate unordered array of flashcards
-		chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards) {
-    	let unordered_cards = JSON.parse(current_flashcards.flashcards);
-    	let temp;
-    	for(temp in unordered_cards) {
-	       pile.push(temp);
-    	}
-			if (pile.length < 10) {
-				correct = pile.length;
-			}
-			else {
-				correct = 10
-			}
-			$("#correct").text(correct);
+	if (pile.length == 0) {
+		//Generate unordered array of flashcards
+			chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards) {
+	    	unordered_cards = JSON.parse(current_flashcards.flashcards);
+	    	let temp;
 
-			ask(pile);
-			console.log("unordered_cards: ", unordered_cards);
-			correct_answer = unordered_cards.question;
-			console.log("correct answer: ", correct_answer);
-		});
+				// Create array of keys from dictionarry
+	    	for(temp in unordered_cards) {
+		       pile.push(temp);
+	    	}
 
-		// Flashcard check
-		$('#check').click(function(){
-			let total_correct;
-			user_answer = $('#answer').val();
-			console.log("You answered: ", user_answer);
-			if (user_answer == correct_answer) {
-				console.log("correct");
-				total_correct++;
-			}
-			if (total_correct == correct) {
-				console.log("You win!");
-			}
-			$("#total_correct").text(total_correct);
-			ask(pile);
-	  });
+				// Define total correct answers to pass
+				if (pile.length < 20) {
+					correct = pile.length;
+				}
+				else {
+					correct = 10
+				}
+				$("#correct").text(correct);
+
+				ask(pile);
+				console.log("cards: ", unordered_cards);
+			});
+		}
+
+	// Flashcard check
+	$('#check').click(function(){
+		user_answer = $('#Answer').val();
+		console.log("You answered: ", user_answer);
+
+		// Check correct
+		if (user_answer == correct_answer) {
+			console.log("correct");
+			pile.splice(splice, 1);
+			total_correct++;
+		}
+
+		// Check if pass
+		if (total_correct == correct) {
+			console.log("You win!");
+			chrome.storage.sync.set({'pass'} : true);
+			chrome.tabs.update({url:old_url});
+		}
+
+		// Clear input
+		$("#Answer").val('');
+
+		// Update counter
+		$("#total_correct").text(total_correct);
+
+		ask(pile);
+  });
 });
