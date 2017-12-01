f75d24857328e07728369f449a162cd458427d7a
Evan <evan1041@me.com>
2017-12-01 03:08:19 -0500

    Error

--- Google/flashcards.js
+++ Google/flashcards.js
@@ -1,12 +1,23 @@
 $(function(){
 	//Generate intial flashcard
-	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  		console.log("ping at", tab.url)
-  	});
+	chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards){
+    	let unordered_cards = JSON.parse(current_flashcards.flashcards);
+    	let pile = [];
+    	let temp;
+    	for(temp in unordered_cards) {
+	        if(unordered_cards.hasOwnProperty(temp)) {
+	           pile.push(temp);
+	       	}
+    	}
+    	initial = obj[pile[Math.floor(Math.random() * pile.length)]];
+    	console.log("initial: ", initial)
+    	pile.splice(initial, 1);
+    	let question = pile[initial];
+
+    });
 
 	// Flashcard functionality
 	$('#check').click(function(){
 	  console.log("ping click")
-
 	});
 });
