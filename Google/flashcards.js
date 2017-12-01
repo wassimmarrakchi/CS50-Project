21f04bb90dcc1f752878333197b429d0a1b31e4b
Wassim Marrakchi <marrakchiwassim@yahoo.fr>
2017-12-01 09:05:57 -0500

    e

--- Google/flashcards.js
+++ Google/flashcards.js
@@ -1,52 +1,58 @@
-// Flashcard functionality
-let pile = [];
-let questions = 10;
-let total_correct = 0;
-let unordered_cards = {};
-
 //Generate unordered array of flashcards
-chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards) {
+chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards)
+{
+	// Flashcard functionality
+	let pile = [];
+	let questions = 10;
+	let total_correct = 0;
+	let unordered_cards = {};
 	unordered_cards = JSON.parse(current_flashcards.flashcards);
 
 	// Create array of keys from dictionary
 	let temp;
-	for(temp in unordered_cards) {
+	for(temp in unordered_cards)
+	{
      pile.push(temp);
 	}
 
-	consol.log("unordered_cards: ", unordered_cards);
+	console.log("unordered_cards: ", unordered_cards);
 
-	if (pile.length = 0) {
+	if (pile.length = 0)
+	{
 		chrome.tabs.update({url:chrome.extension.getURL('error.html')});
 	}
 
 	// Define total correct answers to pass (default 10 unless fewer exist)
-	if (pile.length < 10) {
+	if (pile.length < 10)
+	{
 		questions = pile.length;
+		$("#questions").text(questions);
 	}
 
 	// Render total number of questions
 	$("#questions").text(questions);
+
+	// Generate and display new question
+	while (total_correct < questions)
+	{
+		let location = Math.floor(Math.random() * pile.length);
+		$("#question").text(pile[location]);
+
+		// Check user submission
+		$('#check').click(function()
+		{
+			// Check correct
+			if ($('#answer').val() == unordered_cards[pile[location]])
+			{
+				pile.splice(location, 1);
+				total_correct++;
+			}
+
+			// Clear input
+			$("#Answer").val('');
+
+			// Update counter
+			$("#total_correct").text(total_correct);
+		});
+	};
 });
-
-// Generate and display new question
-while (total_correct < questions) {
-	let location = Math.floor(Math.random() * pile.length);
-	$("#question").text(pile[location]);
-
-	// Check user submission
-	$('#check').click(function() {
-
-		// Check correct
-		if ($('#answer').val() == unordered_cards[pile[location]]) {
-			pile.splice(location, 1);
-			total_correct++;
-		}
-
-		// Clear input
-		$("#Answer").val('');
-
-		// Update counter
-		$("#total_correct").text(total_correct);
-	});
-};
