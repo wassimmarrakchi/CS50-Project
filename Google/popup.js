6a0a31e1547009a2ddb8d75f6314c0c91bdf9521
Wassim Marrakchi <marrakchiwassim@yahoo.fr>
2017-11-30 22:14:44 -0500

    e

--- Google/popup.js
+++ Google/popup.js
@@ -1,31 +1,35 @@
-let blocked =[];
-
-
 $(function(){
 
+  // Display the number of websites blocked near the "Blocked Sites" button
   chrome.storage.sync.get('number', function(blocks){
-    $('#number').text(blocks.number);
+    $('#number').text(parseInt(blocks.number));
+    console.log("The number of websites blocked already is ", blocks.number)
   })
 
+
+  // Block a typed array
   $('#Block').click(function(){
 
-      number = blocked.length;
-
+      chrome.storage.sync.get('number', function(oldNumber){
+        number = parseInt(oldNumber);
+        console.log("The new number of websites blocked is ", number);
+      });
 
       // Add the website to the list of blocked websites and increment by one the
       // number of websites blocked
-      var url = $('#url').val();
+      var url = $('#url').val().toString();
       if(url){
-        blocked.push(url.toString());
         number +=1;
-      }
+        chrome.storage.sync.set({number : url})
+        chrome.storage.sync.set({'number': number});
+        $('#number').text(toString(number));
+        $('#url').val('');
+        };
 
-      for(let i = 0; i < blocked.length; i++){
-        console.log(blocked[i])
-      }
-
-      chrome.storage.sync.set({'number': number});
-      $('#number').text(number);
-      $('#url').val('');
-    });
+        for(let i = 0; i < number; i++) {
+          chrome.storage.sync.get(toString(i), function(result){
+              console.log(result.i);
+          });
+        };
+      });
 });
