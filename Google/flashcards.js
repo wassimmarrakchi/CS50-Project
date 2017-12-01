dafda1efe3c48802093a5a7c8c83580157841a0a
Evan <evan1041@me.com>
2017-12-01 03:17:33 -0500

    ??

--- Google/flashcards.js
+++ Google/flashcards.js
@@ -2,15 +2,16 @@ $(function(){
 	//Generate intial flashcard
 	chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards){
     	let unordered_cards = JSON.parse(current_flashcards.flashcards);
+    	console.log("pairs: ", unordered_cards);
     	let pile = [];
     	let temp;
     	for(temp in unordered_cards) {
-	        if(unordered_cards.hasOwnProperty(temp)) {
-	           pile.push(temp);
-	       	}
+	        pile.push(temp);
+	        console.log("temp: ", temp);
     	}
     	initial = pile[Math.floor(Math.random() * pile.length)];
-    	console.log("initial: ", initial)
+    	console.log("pile: ", pile);
+    	console.log("initial: ", initial);
     	pile.splice(initial, 1);
     	let question = pile[initial];
 
