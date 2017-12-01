0472443ef834d86c1acc972f947829d75a6f5e10
Evan <evan1041@me.com>
2017-12-01 03:27:31 -0500

    dfa

--- Google/flashcards.js
+++ Google/flashcards.js
@@ -1,5 +1,5 @@
 $(function(){
-	//Generate intial flashcard
+	//Generate array of flashcards
 	chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards){
     	let unordered_cards = JSON.parse(current_flashcards.flashcards);
     	console.log("pairs: ", unordered_cards);
@@ -9,17 +9,20 @@ $(function(){
 	        pile.push(temp);
 	        console.log("temp: ", temp);
     	}
-    	initial = pile[Math.floor(Math.random() * pile.length)];
-    	console.log("pile: ", pile);
-    	console.log("initial: ", initial);
-    	pile.splice(initial, 1);
-    	let question = initial;
-        $("#question").text(question);
-
     });
 
-	// Flashcard functionality
+	// Flashcard check
 	$('#check').click(function(){
 	  console.log("ping click")
+      question 
 	});
+
+    function ask() {
+        initial = pile[Math.floor(Math.random() * pile.length)];
+        console.log("pile: ", pile);
+        console.log("initial: ", initial);
+        pile.splice(initial, 1);
+        let question = initial;
+        $("#question").text(question);
+    }
 });
