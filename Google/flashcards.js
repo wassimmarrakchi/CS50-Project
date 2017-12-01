9a4c828a378ef5da1c01f6a50e036c9b69c93b2e
Evan <evan1041@me.com>
2017-12-01 04:10:14 -0500

    c

--- Google/flashcards.js
+++ Google/flashcards.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let pile = [];
 let correct_answer;
 let correct;
@@ -14,20 +13,11 @@ function ask(pile) {
 $(function(){
 	//Generate unordered array of flashcards
 		chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards) {
-=======
-$(function()
-{
-	//Generate intial flashcard
-	chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcards)
-	{
->>>>>>> 978c318388d89c19dd09a7335540816c75e0efde
->>>>>>> 4d86cb17104b7146193051574dbd7f03a1c6f60e
     	let unordered_cards = JSON.parse(current_flashcards.flashcards);
     	let temp;
     	for(temp in unordered_cards) {
 	       pile.push(temp);
     	}
-<<<<<<< HEAD
 			if (pile.length < 10) {
 				correct = pile.length;
 			}
@@ -57,34 +47,4 @@ $(function()
 			$("#total_correct").text(total_correct);
 			ask(pile);
 	  });
-=======
-<<<<<<< HEAD
-    });
-
-	// Flashcard check
-	$('#check').click(function(){
-	  console.log("ping click")
-      question
-=======
-    	initial = pile[Math.floor(Math.random() * pile.length)];
-    	pile.splice(initial, 1);
-    	let question = initial;
-      $("#question").text(question);
-  });
-
-	// Flashcard functionality
-	$('#check').click(function()
-	{
-			console.log("ping click")
-	});
-
-  function ask() {
-        initial = pile[Math.floor(Math.random() * pile.length)];
-        console.log("pile: ", pile);
-        console.log("initial: ", initial);
-        pile.splice(initial, 1);
-        let question = initial;
-        $("#question").text(question);
-    }
->>>>>>> 4d86cb17104b7146193051574dbd7f03a1c6f60e
 });
