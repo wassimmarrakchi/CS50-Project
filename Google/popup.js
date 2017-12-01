5f700ec0d478c395794471f110d050e20bae496e
Wassim Marrakchi <marrakchiwassim@yahoo.fr>
2017-11-30 20:19:50 -0500

    c

--- Google/popup.js
+++ Google/popup.js
@@ -6,19 +6,20 @@ $(function(){
 
       number = blocked.length;
 
-      
       // Add the website to the list of blocked websites and increment by one the
       // number of websites blocked
       var url = $('#url').val();
       if(url){
-        blocked.append(string(url));
+        blocked.push(url.toString());
         number +=1;
       }
 
-      chrome.storage.sync.set({'number': number});
+      for(let i = 0; i < blocked.length; i++){
+        console.log(blocked[i])
+      }
 
+      chrome.storage.sync.set({'number': number});
       $('#number').text(number);
       $('#url').val('');
     });
-  });
 });
