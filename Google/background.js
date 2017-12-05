d2d22214a8b8992824b54e1569c0a542f172a1c4
Wassim Marrakchi <marrakchiwassim@yahoo.fr>
2017-12-04 21:48:48 -0500

    blocck all websites

--- Google/background.js
+++ Google/background.js
@@ -3,11 +3,13 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)
 {
   chrome.storage.sync.get(['number', 'websites'], function(oldNumber)
   {
+    let url = "";
     let number = parseInt(oldNumber.number);
     let websites = JSON.parse(oldNumber.websites);
     for(let i = 0; i < number; i++)
     {
-      if (tab.url == websites[i])
+      url = "^.*" + websites[i] + ".*$";
+      if (tab.url.match(url))
       {
         chrome.tabs.update({url:chrome.extension.getURL('flashcard.html')});
         console.log("Url blocked: ", tab.url);
