6be657200152d18376bcbfcc89512fb918e6bcb9
Evan <evan1041@me.com>
2017-12-04 21:14:37 -0500

    new flashcard.js

--- Google/background.js
+++ Google/background.js
@@ -1,24 +1,17 @@
 // Redirects blocked websites
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)
 {
-  chrome.storage.sync.get(toString(tab.url), function(block)
+  chrome.storage.sync.get(['number', 'websites'], function(oldNumber)
   {
-  	if (block != true)
+    let number = parseInt(oldNumber.number);
+    let websites = JSON.parse(oldNumber.websites);
+    for(let i = 0; i < number; i++)
     {
-      chrome.storage.sync.get(['number', 'websites'], function(oldNumber)
+      if (tab.url == websites[i])
       {
-        let number = parseInt(oldNumber.number);
-        let websites = JSON.parse(oldNumber.websites);
-        for(let i = 0; i < number; i++)
-        {
-          if (tab.url == websites[i])
-          {
-            chrome.storage.sync.set({'visited' : tab.url});
-            chrome.tabs.update({url:chrome.extension.getURL('flashcard.html')});
-            console.log("Url blocked: ", tab.url);
-          };
-        };
-      });
+        chrome.tabs.update({url:chrome.extension.getURL('flashcard.html')});
+        console.log("Url blocked: ", tab.url);
+      };
     };
   });
 });
--- Google/flashcards.js
+++ Google/flashcards.js
@@ -5,8 +5,14 @@ chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcar
 	let pile = [];
 	let questions = 10;
 	let total_correct = 0;
-	let unordered_cards = {};
-	unordered_cards = JSON.parse(current_flashcards.flashcards);
+	let location = 0;
+	let unordered_cards = JSON.parse(current_flashcards.flashcards);
+
+	// Ensure flashcards
+	if (Object.keys(unordered_cards).length == 0)
+	{
+		chrome.tabs.update({url:chrome.extension.getURL('error.html')});
+	}
 
 	// Create array of keys from dictionary
 	let temp;
@@ -16,43 +22,57 @@ chrome.storage.sync.get(['numberFlash', 'flashcards'], function(current_flashcar
 	}
 
 	console.log("unordered_cards: ", unordered_cards);
-
-	if (pile.length = 0)
-	{
-		chrome.tabs.update({url:chrome.extension.getURL('error.html')});
-	}
+	console.log("pile: ", pile);
+	console.log("pile length: ", pile.length);
+	console.log("questions before: ", questions);
 
 	// Define total correct answers to pass (default 10 unless fewer exist)
 	if (pile.length < 10)
 	{
 		questions = pile.length;
-		$("#questions").text(questions);
+		console.log("questions after: ", questions);
 	}
 
 	// Render total number of questions
 	$("#questions").text(questions);
 
-	// Generate and display new question
-	while (total_correct < questions)
+	ask(pile)
+
+	$('#check').click(function()
 	{
-		let location = Math.floor(Math.random() * pile.length);
-		$("#question").text(pile[location]);
-
-		// Check user submission
-		$('#check').click(function()
-		{
-			// Check correct
-			if ($('#answer').val() == unordered_cards[pile[location]])
-			{
-				pile.splice(location, 1);
-				total_correct++;
-			}
-
-			// Clear input
-			$("#Answer").val('');
-
-			// Update counter
-			$("#total_correct").text(total_correct);
-		});
-	};
+		check(total_correct, questions, pile, location, unordered_cards)
+	});
 });
+
+function ask(pile, location)
+{
+	location = Math.floor(Math.random() * pile.length);
+	$("#question").text(pile[location]);
+}
+
+function check(total_correct, questions, pile, location, unordered_cards)
+{
+	console.log("answer: ", unordered_cards[pile[location]]);
+	// Check correct
+	if ($('#answer').val() == unordered_cards[pile[location]])
+	{
+		pile.splice(location, 1);
+		total_correct++;
+		$("#answer").val('');
+		$("#total_correct").text(total_correct);
+		console.log("correct!")
+	}
+	else
+	{
+		$("#answer").text(unordered_cards[pile[location]]);
+	}
+
+	if (total_correct == questions)
+	{
+		chrome.tabs.update({url:chrome.extension.getURL('popup.html')});
+	}
+	else
+	{
+		ask(pile)
+	}
+}
